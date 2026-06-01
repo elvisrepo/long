49d87d671ca377a8d73ef5962aa0b4a38ae913ba
elvisrepo <elvis.dev2@gmail.com>
2026-06-01 12:03:12 +0200

    delete mutation hook

---
+++ frontend/src/features/metrics/use-delete-metric-entry-mutation.ts
@@ -0,0 +1,16 @@
+ import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+import { deleteMetricEntry } from './metric-entries-api'
+
+ export function useDeleteMetricEntryMutation() {
+    const queryClient = useQueryClient()
+
+    return useMutation({
+      mutationFn: (id: number) => deleteMetricEntry(id),
+      onSuccess: () => {
+        return queryClient.invalidateQueries({
+          queryKey: ['metric-entries'],
+        })
+      },
+    })
+  }
